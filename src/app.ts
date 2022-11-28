e5cf332f464031b008041b1b5d434348a0310306
zxiaosi <2692159310@qq.com>
2022-11-28 10:30:02 +0800

    添加全局token

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,13 @@
 import { Component, PropsWithChildren } from 'react'
 import './app.less'
 import { env } from './config';
+import { requestToken } from './request/interceptors';
 
 class App extends Component<PropsWithChildren> {
 
   async componentDidMount() {
     console.log("当前运行环境:", env);
+    // await requestToken(); // 提前获取并保存token, 防止存储过慢导致的问题
   }
 
   componentDidShow() { }
